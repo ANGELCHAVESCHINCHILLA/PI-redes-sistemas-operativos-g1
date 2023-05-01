4e718217b317d8b4b1ea75ceda22c78989ec4460
DavidC <david.cerdasalvarado@ucr.ac.cr>
2023-05-01 00:49:06 -0600

    feat: Added scripts.js: a simple way to handle imports of js libraries

---
+++ WEB_UI/scripts.js
@@ -0,0 +1,15 @@
+/**
+ * This class is a way to simplify the scripts imports. Just add this script to a HTML
+ * and this script wil to add the rest of scripts.
+ */
+
+
+
+// Numbers to letters
+document.write('<script src="libs/NumeroALetras.js"></script>');
+
+// App 
+document.write('<script src="app.js"></script>');
+
+// Modals
+document.write('<script src="modal.js"></script>');
