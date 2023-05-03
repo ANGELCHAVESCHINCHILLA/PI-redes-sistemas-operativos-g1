802608d621d1a428d7fdb075c1b66eefbab40b20
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2023-05-03 15:20:09 -0600

    Test append

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,8 @@
 int main(int, char**) {
   FS fs;
 
-  try {
-    if (fs.create("a.dat") == BLOCK_UNDEFINED) {
-      throw std::runtime_error("There is no space in the directory.");
-    }
-  } catch (const std::runtime_error& e) {
-    std::cerr << e.what() << "\n";
-  }
+  fs.create("a.dat");
+  fs.append("a.dat", 'a');
 
   std::cout << fs.toString() << "\n";
 }
