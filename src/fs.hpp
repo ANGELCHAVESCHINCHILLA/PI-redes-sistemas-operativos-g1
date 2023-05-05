e12576deb0ae52d7864d1fae23dc8c3a6ec3abd2
Angel Chaves Chinchilla <ANGEL.CHAVESCHINCHILLA@ucr.ac.cr>
2023-05-05 12:45:52 -0600

    Documentation: Copyright messages in source files

--- src/fs.hpp
+++ src/fs.hpp
@@ -1,4 +1,8 @@
-//
+// @Copyright 2023 Universidad de Costa Rica
+// Ángel Chaves Chinchilla (c12113) angel.chaveschichilla@ucr.ac.cr
+// David Cerdas Alvarado (C02001) david.cerdasalvarado@ucr.ac.cr
+// Camilo Suárez Sandí (C17811) camilo.suarez@ucr.ac.cr
+// Ignacio Robles Mayorga (B96549) ignacio.robles@ucr.ac.cr
 
 #include <string>
 
