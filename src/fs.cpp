c99082c0480119978fcbffc85b32985932103874
Angel Chaves Chinchilla <ANGEL.CHAVESCHINCHILLA@ucr.ac.cr>
2023-05-12 13:19:16 -0600

    Code Filesystem: append and toString methods improved and read method added

---
+++ src/fs.cpp
@@ -0,0 +1,381 @@
+// @Copyright 2023 Universidad de Costa Rica
+// Ángel Chaves Chinchilla (C12113) angel.chaveschichilla@ucr.ac.cr
+// David Cerdas Alvarado (C02001) david.cerdasalvarado@ucr.ac.cr
+// Camilo Suárez Sandí (C17811) camilo.suarez@ucr.ac.cr
+// Ignacio Robles Mayorga (B96549) ignacio.robles@ucr.ac.cr
+
+#include "fs.hpp"
+
+#include <time.h>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+
+#include "error.hpp"
+
+DirectoryEntry::DirectoryEntry() : startBlock(DIRECTORY_UNDEFINED) {
+  //
+}
+
+DirectoryEntry::~DirectoryEntry() {
+  //
+}
+
+DirectoryEntry::DirectoryEntry(int block, std::string name, char *permissions,
+                               std::string belongsFolder)
+    : startBlock(block), name(name), permissions(permissions),
+      belongsFolder(belongsFolder) {
+  this->date = ::time(0);
+}
+
+void DirectoryEntry::reset() {
+  this->startBlock = FAT_UNDEFINED;
+  this->name = "";
+  this->date = 0;
+}
+
+FS::FS() {
+  this->directory = new DirectoryEntry[DIRECTORY_COUNT];
+  this->fat = new int[FAT_COUNT];
+  this->unit = new char[FAT_COUNT * BLOCK_SIZE];
+
+  // Initialize the fat table to undefined
+  ::memset(this->fat, FAT_UNDEFINED, FAT_COUNT * sizeof(int));
+
+  // Initialize the unit to empty
+  ::memset(this->unit, (char)BLOCK_EOF, FAT_COUNT * BLOCK_SIZE);
+}
+
+FS::~FS() {
+  delete[] this->directory;
+  delete[] this->fat;
+  delete[] this->unit;
+}
+
+int FS::create(std::string name) {
+  int error = Error::OK;
+  // Find a space in the directory
+  const int directory_index = this->findDirectorySpace();
+
+  if (directory_index != DIRECTORY_UNDEFINED) {
+    // Find a space in the fat
+    const int fat_index = this->findFATSpace();
+
+    if (fat_index != FAT_UNDEFINED) {
+      DirectoryEntry directory_entry(fat_index, name);
+
+      this->directory[directory_index] = directory_entry;
+
+      this->fat[fat_index] = FAT_RESERVED;
+    } else {
+      error = Error::NO_SPACE_IN_FAT;
+    }
+  } else {
+    error = Error::NO_SPACE_IN_DIRECTORY;
+  }
+
+  if (!error) {
+    return directory_index;
+  } else {
+    return error;
+  }
+}
+
+// ¿Deberíamos controlar que el parámetro character no sea el caracter nulo?
+int FS::append(std::string name, char character) {
+  int error = Error::OK;
+
+  // Search if the file exists in the directory
+  const int directory_index = this->searchFile(name);
+
+  if (directory_index == DIRECTORY_UNDEFINED) {
+    error = Error::FILE_NOT_FOUND;
+  }
+
+  if (!error) {
+    const int start_block = this->directory[directory_index].startBlock;
+
+    // If is the first time that a character will be added to the file
+    if (this->fat[start_block] == FAT_RESERVED) {
+      // Write the data
+      this->unit[start_block * BLOCK_SIZE] = character;
+      // The next character is set as EOF
+      this->unit[start_block * BLOCK_SIZE + 1] = BLOCK_EOF;
+      // Set the EOF in the FAT
+      this->fat[start_block] = FAT_EOF;
+      // If the file already had at least one character
+    } else {
+      // Find the EOF in FAT
+      const int fat_index = this->searchEOF(start_block);
+      if (this->fat[fat_index] == FAT_EOF) {
+        // Find the EOF in the block
+        int index = 0;
+        while (this->unit[fat_index * BLOCK_SIZE + index] != BLOCK_EOF &&
+               index < BLOCK_SIZE) {
+          ++index;
+        }
+        const int block_index = index;
+
+        // Write the data (a character) where the EOF was storaged
+        this->unit[fat_index * BLOCK_SIZE + block_index] = character;
+
+        // If there is not space in the block
+        if (block_index == BLOCK_SIZE - 1) {
+          // Find a new free block in FAT
+          const int next_fat_index = this->findFATSpace();
+          if (next_fat_index == FAT_UNDEFINED) {
+            error = Error::NO_SPACE_IN_FAT;
+          }
+
+          if (!error) {
+            // Update the EOF In FAT
+            this->fat[fat_index] = next_fat_index;
+            this->fat[next_fat_index] = FAT_EOF;
+
+            // Set the EOF in the unit
+            this->unit[next_fat_index * BLOCK_SIZE] = BLOCK_EOF;
+          }
+
+          // If there's space in the block
+        } else if (index < BLOCK_SIZE - 1) {
+          // Set the EOF in the unit
+          this->unit[fat_index * BLOCK_SIZE + block_index + 1] = BLOCK_EOF;
+        } else {
+          error = Error::NOT_OK;
+        }
+      } else {
+        error = Error::INVALID_FILE;
+      }
+    }
+  }
+  return error;
+}
+
+char FS::read(std::string fileName, size_t position, char *permissions) {
+  int error = Error::OK;
+  int directory_index = this->searchFile(fileName);
+  if (directory_index == DIRECTORY_UNDEFINED) {
+    error = Error::FILE_NOT_FOUND;
+  }
+
+  if (!error) {
+    // start block of the file
+    const int start_block = this->directory[directory_index].startBlock;
+    // The amount of blocks to to traverse to reach the desired character
+    const int block_number = position / static_cast<int>(BLOCK_SIZE);
+    // Number of positions to go through in the block to reach the desired
+    // character
+    const int char_number = position - block_number * BLOCK_SIZE;
+
+    // Find the block in which is stored the character
+    int fat_index = start_block;
+    int block_count = 0;
+    while (block_count < block_number) {
+      fat_index = this->fat[fat_index];
+      ++block_count;
+    }
+
+    // Find the char in the block
+    int char_index = 0;
+    while (this->unit[fat_index * BLOCK_SIZE + char_index] != BLOCK_EOF &&
+           char_index < char_number) {
+      ++char_index;
+    }
+
+    const char result = this->unit[fat_index * BLOCK_SIZE + char_index];
+    return result;
+  } else {
+    return '\0';
+  }
+}
+
+int FS::remove(std::string name) {
+  //
+  return this->privateRemove(name, false);
+}
+
+int FS::deepRemove(std::string name) {
+  //
+  return this->privateRemove(name, true);
+}
+
+int FS::privateRemove(std::string name, bool deep) {
+  int error = Error::OK;
+
+  int directory_index = this->searchFile(name);
+
+  if (directory_index == DIRECTORY_UNDEFINED) {
+    error = Error::FILE_NOT_FOUND;
+  }
+
+  if (!error) {
+    // Remove each block of the FAT assigned to the file.
+    int fat_index = this->directory[directory_index].startBlock;
+    int tmp_fat_index = fat_index;
+
+    while (this->fat[fat_index] != FAT_EOF &&
+           this->fat[fat_index] != FAT_UNDEFINED &&
+           this->fat[fat_index] != FAT_RESERVED && fat_index < FAT_COUNT) {
+      // Update fat index
+      fat_index = this->fat[fat_index];
+      // Remove FAT data
+      this->fat[tmp_fat_index] = FAT_UNDEFINED;
+
+      // Remove unit data
+      if (deep) {
+        ::memset(&this->unit[tmp_fat_index], BLOCK_EOF, BLOCK_SIZE);
+      }
+
+      // Update temporal fat index
+      tmp_fat_index = fat_index;
+    }
+
+    if (this->fat[fat_index] != FAT_EOF &&
+        this->fat[fat_index] != FAT_RESERVED) {
+      error = Error::INVALID_FILE;
+    }
+
+    if (!error) {
+      // Remove the EOF
+      this->fat[fat_index] = FAT_UNDEFINED;
+
+      // Remove unit at last block
+      if (deep) {
+        ::memset(&this->unit[fat_index], BLOCK_EOF, BLOCK_SIZE);
+      }
+
+      // Remove directory entry
+      this->directory[directory_index].reset();
+    }
+  }
+
+  return error;
+}
+
+int FS::findDirectorySpace() {
+  int index = 0;
+  bool found = false;
+
+  while (index < DIRECTORY_COUNT && !found) {
+    if (this->directory[index].startBlock == DIRECTORY_UNDEFINED) {
+      found = true;
+      break;
+    } else {
+      index++;
+    }
+  }
+
+  if (!found) {
+    index = DIRECTORY_UNDEFINED;
+  }
+
+  return index;
+}
+
+int FS::findFATSpace() {
+  int index = 0;
+  bool found = false;
+
+  while (index < FAT_COUNT && !found) {
+    if (this->fat[index] == FAT_UNDEFINED) {
+      found = true;
+      break;
+    } else {
+      ++index;
+    }
+  }
+
+  if (!found) {
+    index = FAT_UNDEFINED;
+  }
+
+  return index;
+}
+
+int FS::searchEOF(int index) {
+  int fat_index = index;
+
+  while (this->fat[fat_index] != FAT_EOF &&
+         this->fat[fat_index] != FAT_UNDEFINED && fat_index < FAT_COUNT) {
+    fat_index = this->fat[fat_index];
+  }
+
+  return fat_index;
+}
+
+int FS::searchFile(std::string name) {
+  int index = 0;
+  bool found = false;
+
+  while (index < DIRECTORY_COUNT && !found) {
+    DirectoryEntry entry = this->directory[index];
+
+    if (entry.name == name) {
+      found = true;
+      break;
+    } else {
+      ++index;
+    }
+  }
+
+  if (!found) {
+    index = DIRECTORY_UNDEFINED;
+  }
+
+  return index;
+}
+
+std::string FS::toString() {
+  std::stringstream ss;
+
+  ss << "Directory:\n";
+
+  for (size_t index = 0; index < DIRECTORY_COUNT; ++index) {
+    if (this->directory[index].name != "") {
+      ss << "\"" << this->directory[index].name << "\" "
+         << this->directory[index].startBlock << "\n";
+    }
+  }
+
+  ss << "\nFAT:\n";
+
+  for (size_t index = 0; index < FAT_COUNT; ++index) {
+    ss << index << " ";
+  }
+
+  ss << "\n";
+
+  for (size_t index = 0; index < FAT_COUNT; ++index) {
+    const int entry = this->fat[index];
+
+    if (entry == FAT_UNDEFINED) {
+      ss << "_ ";
+    } else if (entry == FAT_RESERVED) {
+      ss << "R ";
+    } else if (entry == FAT_EOF) {
+      ss << "E ";
+    } else {
+      ss << entry << " ";
+    }
+  }
+
+  ss << "\n\nBlocks:\n";
+
+  for (size_t index = 0; index < FAT_COUNT; ++index) {
+    ss << index << " ";
+    for (size_t unit_index = 0; unit_index < BLOCK_SIZE; ++unit_index) {
+      char data = this->unit[unit_index + index * BLOCK_SIZE];
+
+      if (data == '\0') {
+        ss << "_ ";
+      } else {
+        ss << data << " ";
+      }
+    }
+    ss << "\n";
+  }
+
+  ss << "\n";
+
+  return ss.str();
+}
---
+++ src/fs.hpp
@@ -0,0 +1,206 @@
+// @Copyright 2023 Universidad de Costa Rica
+// Ángel Chaves Chinchilla (c12113) angel.chaveschichilla@ucr.ac.cr
+// David Cerdas Alvarado (C02001) david.cerdasalvarado@ucr.ac.cr
+// Camilo Suárez Sandí (C17811) camilo.suarez@ucr.ac.cr
+// Ignacio Robles Mayorga (B96549) ignacio.robles@ucr.ac.cr
+
+#include <string>
+
+#define DIRECTORY_COUNT 8
+#define DIRECTORY_UNDEFINED -1
+
+#define FAT_COUNT 8
+#define FAT_UNDEFINED -1
+#define FAT_RESERVED -2
+#define FAT_EOF -3
+
+#define BLOCK_SIZE 64
+#define BLOCK_EOF '\0'
+
+struct DirectoryEntry {
+ public:
+  /**
+   * @brief The position of the first block in the FAT.
+   *
+   */
+  int startBlock;
+
+  /**
+   * @brief The name of the file.
+   *
+   */
+  std::string name;
+
+  /**
+   * @brief The time the file was created.
+   *
+   */
+  time_t date;
+
+  /**
+  */
+  std::string owner;
+
+  /**
+  */
+  char* permissions;
+
+  /**
+  */
+  std::string belongsFolder;
+
+  /**
+  */
+  bool isFile;
+
+  /**
+   * @brief Default constructor.
+   *
+   */
+  DirectoryEntry();
+
+  /**
+   * @brief Constructor.
+   *
+   */
+  ~DirectoryEntry();
+
+  /**
+   * @brief Construct a directory entry with a block, name and the current time.
+   *
+   * @param block The index of the block.
+   * @param name The name of the file.
+   */
+  DirectoryEntry(int block, std::string name, char* permissions = "111", std::string belongsFolder = "");
+
+  /**
+   * @brief Resets all the properties of the entry.
+   *
+   */
+  void reset();
+};
+
+class FS {
+ private:
+  /**
+   * @brief An array that stores the directory entries.
+   *
+   */
+  DirectoryEntry* directory;
+
+  /**
+   * @brief The file allocation table.
+   *
+   */
+  int* fat;
+
+  /**
+   * @brief The data of the files.
+   *
+   */
+  char* unit;
+
+ public:
+  /**
+   * @brief Default constructor.
+   *
+   */
+  FS();
+
+  /**
+   * @brief Destructor.
+   *
+   */
+  ~FS();
+
+  /**
+   * @brief Creates a file.
+   *
+   * @param name The name of the file.
+   * @return int The position of the first block of the file created or an error
+   * code.
+   */
+  int create(std::string name);
+
+  /**
+   * @brief Adds data to a file.
+   *
+   * @param name The name of the file.
+   * @param character The data.
+   * @return int An error code.
+   */
+  int append(std::string name, char character);
+
+  /**
+  */
+  char read(std::string fileName, size_t position, char *permissions);
+
+  /**
+   * @brief Delete the file, the unit of storage will have garbage.
+   *
+   * @param name The name of the file.
+   * @return int An error code.
+   */
+  int remove(std::string name);
+
+  /**
+   * @brief Delete the file, the unit of storage will be emptied.
+   *
+   * @param name The name of the file.
+   * @return int An error code.
+   */
+  int deepRemove(std::string name);
+
+  /**
+   * @brief Returns the contents of the file system.
+   *
+   * @return std::string The contents of the file system.
+   */
+  std::string toString();
+
+ private:
+  /**
+   * @brief Delete the file. If deep is set to true the unit of storage will be
+   * emptied.
+   *
+   * @param name The name of the file.
+   * @param deep If the unit of storage will be emptied.
+   * @return int An error code.
+   */
+  int privateRemove(std::string name, bool deep);
+
+  /**
+   * @brief Returns the position of a free space in the directory.
+   *
+   * @return int The position of a free space in the directory.
+   */
+  int findDirectorySpace();
+
+  /**
+   * @brief Returns the position of a free space in the FAT.
+   *
+   * @return The position of a free space in the FAT.
+   */
+  int findFATSpace();
+
+  /**
+   * @brief Find the EOF in the FAT starting at @a index. Index must be a valid
+   * start block.
+   *
+   * @param index Start block of a file.
+   * @return int If successful it will return the index of EOF in the FAT,
+   * otherwide it returns FAT_UNDEFINED.
+   */
+  int searchEOF(int index);
+
+  /**
+   * @brief Search the file in directory and return its index in that directory.
+   *
+   * @param name the file name
+   * @return int If the file exists then it returns its index in the directory.
+   * Otherwise it returns an error code.
+   */
+  int searchFile(std::string name);
+
+  std::string& validateUser(const std::string& userName, const std::string& hashKey);
+};
