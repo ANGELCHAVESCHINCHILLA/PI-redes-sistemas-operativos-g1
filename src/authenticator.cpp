afeb1c133d4ffc552302c3bfe5b01442610beff9
Angel Chaves Chinchilla <ANGEL.CHAVESCHINCHILLA@ucr.ac.cr>
2023-05-27 18:32:17 -0600

    Authenticator function added

--- src/authenticator.cpp
+++ src/authenticator.cpp
@@ -1,18 +1,69 @@
+#include <iostream>
+
 #include "authenticator.hpp"
 #include "hash.hpp"
-#include "iostream"
+#include "./common/Util.hpp"
 
-Authenticator::Authenticator() {
-    this->fs = new FS();
-}
+const std::string Authenticator::PEPPER = "Universidad de Costa Rica";
+
+// Authenticator::Authenticator() {
+//     this->fs = new FS();
+// }
+
 Authenticator::Authenticator(FS* fs) {
     this->fs = fs;
 }
 
 Authenticator::~Authenticator() {
-    delete this->fs;
+    // delete this->fs;
 }
-bool Authenticator::authUser(const std::string& username, const std::string& hashKey) {
-   /** TODO(any): do this*/
+
+int Authenticator::authUser(const std::string& usersFile
+    , const std::string& username, const std::string& password) {
+  int error = Error::OK;
+
+  bool found_user = false;
+
+  size_t user_offset = 0;
+
+  const size_t user_bytes = 41;
+
+  while (!found_user &&
+        user_offset < static_cast<size_t>(this->fs->getFileSize(usersFile))) {
+    char* address = this->fs->readAddress(usersFile, user_offset);
+
+    std::string info(address, address + user_bytes);
+    // info username is the username stored in FileSystem
+    std::string info_username = info.substr(0, 10);
+    Util::trimLeft(info_username);
+
+    // If the user name was found in FS then
+    if (username == info_username) {
+      // This ise the hash stored in file system for the user
+      std::string info_hashed_password = info.substr(10, 15);
+      Util::trimLeft(info_hashed_password);
+      // This is the salt store in the file system for the user
+      std::string info_salt = info.substr(25, 15);
+
+      // Calculate the hash with the input password and the salt stored in FS
+      std::string hashed_password =
+          Hash::getString(password, info_salt, Authenticator::PEPPER);
+
+      if (hashed_password == info_hashed_password) {
+        error = Error::OK;
+      } else {
+        error = Error::INVALID_PASSWORD;
+      }
+
+      found_user = true;
+    }
+
+    user_offset += user_bytes;
+  }
+
+  if (!found_user) {
+    error = Error::USER_NOT_FOUND;
+  }
+  return error;
 }
 
--- src/authenticator.hpp
+++ src/authenticator.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "fs.hpp"
+#include "error.hpp"
+
 /**
  * @brief This class contributes to handling the authentication.
  * The idea is use this class using sockets communication or CLI.
@@ -12,16 +14,22 @@ class Authenticator {
   FS* fs;
 
  public:
+  static const std::string PEPPER;
   /// @brief Default Constructor
-  Authenticator();
+  // Authenticator();
   /// @brief Default Destructor
   ~Authenticator();
   /// @brief Constructs an auth using a predefined fs
   /// @param fs predefined fs
   Authenticator(FS* fs);
-  /// @brief Authenticates a user by their username and hashkey.
+  /// @brief Authenticates a user by their username and password.
   /// @param userName user's username to be identificate
-  /// @param hashKey user's hashkey
-  /// @return true if auth. False otherwise.
-  bool authUser(const std::string& username, const std::string& hashKey);
-}; // end class Authenticator
+  /// @param password user's password
+  /// @param usersFile the filename of the users file
+  /// @return error code:
+  /// OK if auth
+  /// INVALID_PASSWORD if the password is invalid
+  /// USER_NOT_FOUND -2 if the users was not found.
+  int authUser(const std::string& usersFile, const std::string& userFame,
+    const std::string& password);
+};  // end class Authenticator
