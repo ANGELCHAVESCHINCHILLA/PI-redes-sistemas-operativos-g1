5fcfc1cf0d52077e6e6a86b9dd7223a18685ee9c
Angel Chaves Chinchilla <ANGEL.CHAVESCHINCHILLA@ucr.ac.cr>
2023-05-28 14:57:54 -0600

    Open socketserver using getaddrinfo() firts and then socket()

--- src/net/TcpServer.cpp
+++ src/net/TcpServer.cpp
@@ -1,9 +1,16 @@
 // Copyright © 2023 Camilo Suárez Sandí, Ángel Chaves Chinchilla
 
+#include <cassert>
+#include <iostream>
+
 #include "TcpServer.hpp"
 
 TcpServer::TcpServer() {
   size_t count = std::thread::hardware_concurrency() / 2;
+
+  this->serverSocketSettings.ai_flags = AI_PASSIVE;  // Fill with my local IP
+  this->serverSocketSettings.ai_family = AF_UNSPEC;
+  this->serverSocketSettings.ai_socktype = SOCK_STREAM; 
 
   this->thread_pool.start(count);
 }
@@ -74,26 +81,84 @@ TcpServer::~TcpServer() {
 // }
 
 int TcpServer::acceptConnections () {
+  std::cout << "Voy a aceptar conexiones";
   int error = SocketError::OK;
 
   while (!error) {
     Socket client_socket;
+    // std::cout << "Logré crear un socket con el cliente conexiones\n";
 
     if (!error) {
-      error = this->server_socket.accept(client_socket);
+      //   std::cout << "Voy a intentar establecer conexi[on]";
+        error = this->server_socket.accept(client_socket);
     }
 
     std::string request;
     std::string response;
 
     if (!error) {
+      std::cout << "Logre establecer conexion con "
+        << client_socket.getFileDescriptor() << std::endl;
       error = client_socket.receive(request);
+    } else {
+      std::cout << "No Logre establecer conexion";
+      
     }
 
     if (!error) {
       this->handleClientConnection(request, response, client_socket);
 
       error = client_socket.send(response);
+    }
+  }
+
+  return error;
+}
+
+int TcpServer::fetchPossibleAddresses(const char* port) {
+  // This function is designed to be called once
+  assert(this->possibleAddresses == nullptr);
+  // Fetch all available addresses and store results in object attributes
+  int error = ::getaddrinfo(nullptr, port, &this->serverSocketSettings
+    , &this->possibleAddresses);
+  // If getaddrinfo failed, raise an exception
+  if (error) {
+    error = SocketError::CANT_FETCH_ADDRESSES;
+  } else {
+    error = SocketError::OK;
+  }
+  return error;
+}
+
+int TcpServer::openServerSocket() {
+  int error = SocketError::OK;
+  assert(this->server_socket.getFileDescriptor() == -1);
+  // Traverse the available addresses and select the first one that works
+  for (const struct addrinfo* address = this->possibleAddresses; address;
+      address = address->ai_next) {
+    // Try to open a socket using this address result
+    error = this->server_socket.create(address);
+
+    // If we could open the socket
+    if (!error) {
+      // Allow the socket to reuse the local IP for other connections
+      error = this->server_socket.allowReuse();
+      if (!error) {
+        // Bind the socket to the port we passed in to getaddrinfo()
+        error = this->server_socket.bind(address);
+        if (!error) {
+          // This address is OK, stop searching and use its socket
+          // this->selectedAddress = address;
+          return error;
+        }
+      } else {
+        throw std::runtime_error("could not set reusing socket");
+      }
+
+      // We could not bind a port for this address, continue searching
+      this->server_socket.close();
+    } else {
+      // We could not open a socket for this address, continue searching
     }
   }
 
