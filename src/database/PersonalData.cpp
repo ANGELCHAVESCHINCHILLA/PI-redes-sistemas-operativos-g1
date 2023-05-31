c79e0cdedf419bbb73559e679aba895cf0665d61
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2023-05-31 17:40:31 -0600

    Create PersonalData table class

---
+++ src/database/PersonalData.cpp
@@ -0,0 +1,103 @@
+// Copyright © 2023 Camilo Suárez Sandí
+
+#include "PersonalData.hpp"
+
+#include <sstream>
+
+const std::string PersonalData::CREATE_TABLE_QUERY =
+    R"(CREATE TABLE PersonalData (
+    User CHAR(16),
+    EmployeeName CHAR(32),
+    JobName CHAR(32),
+    CompanyName CHAR(32),
+    Email CHAR(32),
+    PhoneNumber INT
+);
+)";
+
+const std::string PersonalData::INSERT_INTO_QUERY =
+    R"(INSERT INTO PersonalData (User, EmployeeName, JobName, CompanyName, Email, PhoneNumber) )";
+
+const std::string& PersonalData::getUser() const {
+  return this->user;
+}
+
+const std::string& PersonalData::getEmployeeName() const {
+  return this->employee_name;
+}
+
+const std::string& PersonalData::getJobName() const {
+  return this->job_name;
+}
+
+const std::string& PersonalData::getCompanyName() const {
+  return this->company_name;
+}
+
+const std::string& PersonalData::getEmail() const {
+  return this->email;
+}
+
+int PersonalData::getPhoneNumber() const {
+  return this->phone_number;
+}
+
+std::string PersonalData::getInsertIntoQuery() const {
+  std::stringstream ss;
+
+  ss << PersonalData::INSERT_INTO_QUERY;
+  ss << "VALUES ('";
+  ss << this->user;
+  ss << "', '";
+  ss << this->employee_name;
+  ss << "', '";
+  ss << this->job_name;
+  ss << "', '";
+  ss << this->company_name;
+  ss << "', '";
+  ss << this->email;
+  ss << "', ";
+  ss << this->phone_number;
+  ss << ");";
+
+  return std::move(ss.str());
+}
+
+PersonalData::Builder& PersonalData::Builder::setUser(
+    const std::string& value) {
+  this->object.user = value;
+  return *this;
+}
+
+PersonalData::Builder& PersonalData::Builder::setEmployeeName(
+    const std::string& value) {
+  this->object.employee_name = value;
+  return *this;
+}
+
+PersonalData::Builder& PersonalData::Builder::setJobName(
+    const std::string& value) {
+  this->object.job_name = value;
+  return *this;
+}
+
+PersonalData::Builder& PersonalData::Builder::setCompanyName(
+    const std::string& value) {
+  this->object.company_name = value;
+  return *this;
+}
+
+PersonalData::Builder& PersonalData::Builder::setEmail(
+    const std::string& value) {
+  this->object.email = value;
+  return *this;
+}
+
+PersonalData::Builder& PersonalData::Builder::setPhoneNumber(int value) {
+  this->object.phone_number = value;
+  return *this;
+}
+
+PersonalData PersonalData::Builder::build() {
+  return std::move(this->object);
+}
---
+++ src/database/PersonalData.hpp
@@ -0,0 +1,68 @@
+// Copyright © 2023 Camilo Suárez Sandí
+
+#ifndef PERSONAL_DATA_HPP_
+#define PERSONAL_DATA_HPP_
+
+#include <string>
+
+class PersonalData {
+ private:
+  static const std::string CREATE_TABLE_QUERY;
+
+  static const std::string INSERT_INTO_QUERY;
+
+  std::string user;
+  std::string employee_name;
+  std::string job_name;
+  std::string company_name;
+  std::string email;
+  int phone_number;
+
+  // Constructor
+  PersonalData() = default;
+
+  // Destructor
+  ~PersonalData() = default;
+
+  // Copy Constructor
+  PersonalData(const PersonalData& other) = delete;
+
+  // Copy Assignment Operator
+  PersonalData& operator=(const PersonalData& other) = delete;
+
+  // Move Constructor
+  PersonalData(PersonalData&& other) = default;
+
+  // Move Assignment Operator
+  PersonalData& operator=(PersonalData&& other) = default;
+
+ public:
+  const std::string& getUser() const;
+  const std::string& getEmployeeName() const;
+  const std::string& getJobName() const;
+  const std::string& getCompanyName() const;
+  const std::string& getEmail() const;
+  int getPhoneNumber() const;
+
+  std::string getInsertIntoQuery() const;
+
+  // Inner Class
+  class Builder;
+};
+
+class PersonalData::Builder {
+ private:
+  PersonalData object;
+
+ public:
+  Builder& setUser(const std::string& value);
+  Builder& setEmployeeName(const std::string& value);
+  Builder& setJobName(const std::string& value);
+  Builder& setCompanyName(const std::string& value);
+  Builder& setEmail(const std::string& value);
+  Builder& setPhoneNumber(int value);
+
+  PersonalData build();
+};
+
+#endif  // PERSONAL_DATA_HPP_
