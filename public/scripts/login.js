46d757c566617727a9f9cba32939da6ca5965690
Angel Chaves Chinchilla <ANGEL.CHAVESCHINCHILLA@ucr.ac.cr>
2023-06-01 19:44:57 -0600

    Login post and save a token in localstorage

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -9,39 +9,42 @@ if (login) {
   
       if (username && password) { // Check if exists
 
-        // // Crear un objeto con los datos que deseas enviar
-        // var userValidationInfo = {
-        //   username: username,
-        //   password: password
-        // };
+        // Crear un objeto con los datos que deseas enviar
+        var userValidationInfo = {
+          username: username,
+          password: password
+        };
 
-        // // Realizar la solicitud POST utilizando fetch()
-        // fetch("/login", {
-        //   method: "POST",
-        //   headers: {
-        //     "Content-Type": "application/json"
-        //   },
-        //   body: JSON.stringify(userValidationInfo)
-        // })
+        // Envía los datos de usuario y contraseñ
+        fetch('/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        })
+          // Si la respuesta tiene código de estado 200 entonces continúa, si no
+          // lanza un error
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              alert("El usuario y/o la contraseña son incorrectos");
+            }
+          })
+          // Si viene con un token entonces redirigimos, si no lanzamos un error
+          // en consola.
+          .then(data => {
+            const token = data.token;
+            localStorage.setItem('token', token);
+            window.location.href = 'index.html';
+          })
+          .catch(error => {
+            // Manejar errores de autenticación
+            console.log(error.message);
+          });
 
-        // .then(function(response) {
-        //   if (response.ok) {
-        //     // La solicitud se completó con éxito
-        //     return response.text();
-        //   } else {
-        //     // Ocurrió un error en la solicitud
-        //     throw new Error(response.status);
-        //   }
-        // })
-
-        // .then(function(responseText) {
-        //   console.log(responseText);
-        // })
-        // .catch(function(error) {
-        //   console.error(error);
-        // });
-
-        window.location.href = 'index.html'; // redirection
+        // window.location.href = 'index.html'; // redirection
         //window.location.href = 'form_solicitar_vacaciones.html'; // Redirige a otra página
       } else {
         alert('Por favor complete todos los campos'); // 
