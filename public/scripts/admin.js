50fc7967d954892daecb2627813d84ad3de5693d
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2023-06-09 14:38:29 -0600

    Connect edit and remove user in the front end with the back end

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -17,7 +17,7 @@ const getSalt = (length) => {
     return salt;
 }
 
-const hash = (password, salt) => {
+const getHash = (password, salt) => {
     let hash = CryptoJS.SHA256(password + salt + PEPPER);
 
     hash = hash.toString(CryptoJS.enc.Base64);
@@ -33,46 +33,105 @@ go_back_button.addEventListener('click', () => {
 
 // Add user
 
-const user_send_button = document.querySelector("#user_send_button");
+const add_user_send_button = document.querySelector("#add_user_send_button");
 
-user_send_button.addEventListener('click', async () => {
-    const user_id = document.querySelector("#user_id");
-    const user_password = document.querySelector("#user_password");
-    const user_name = document.querySelector("#user_name");
-    const user_job_name = document.querySelector("#user_job_name");
-    const user_email = document.querySelector("#user_email");
-    const user_phone_number = document.querySelector("#user_phone_number");
-    const user_type = document.querySelector("#user_type");
+if (add_user_send_button !== null) {
+    add_user_send_button.addEventListener('click', async () => {
+        const user_id = document.querySelector("#user_id");
+        const user_password = document.querySelector("#user_password");
+        const user_name = document.querySelector("#user_name");
+        const user_job_name = document.querySelector("#user_job_name");
+        const user_email = document.querySelector("#user_email");
+        const user_phone_number = document.querySelector("#user_phone_number");
+        const user_type = document.querySelector("#user_type");
 
-    const database_json = {
-        user: user_id.value,
-        employee_name: user_name.value,
-        job_name: user_job_name.value,
-        company_name: COMPANY_NAME,
-        email: user_email.value,
-        phone_number: parseInt(user_phone_number.value),
-    };
+        const database_json = {
+            user: user_id.value,
+            employee_name: user_name.value,
+            job_name: user_job_name.value,
+            company_name: COMPANY_NAME,
+            email: user_email.value,
+            phone_number: parseInt(user_phone_number.value),
+        };
 
-    await fetch("/admin/add_user/data", {
-        method: "POST",
-        body: JSON.stringify(database_json)
+        await fetch("/admin/add_user/data", {
+            method: "POST",
+            body: JSON.stringify(database_json)
+        });
+
+        const salt = getSalt(15);
+
+        const fs_json = {
+            "username": user_id.value,
+            "salt": salt,
+            "password": getHash(user_password, salt),
+            "type": parseInt(user_type.value)
+        };
+
+        await fetch("/admin/add_user/auth", {
+            method: "POST",
+            body: JSON.stringify(fs_json)
+        });
+
+        location.href = "/admin";
     });
+}
 
-    const salt = getSalt(15);
-
-    const fs_json = {
-        "username": user_id.value,
-        "salt": salt,
-        "password": hash(user_password, salt),
-        "type": parseInt(user_type.value)
-    };
-
-    await fetch("/admin/add_user/auth", {
-        method: "POST",
-        body: JSON.stringify(fs_json)
-    });
-});
 
 // Remove user
 
+const remove_user_send_button = document.querySelector("#remove_user_send_button");
+
+if (remove_user_send_button !== null) {
+    remove_user_send_button.addEventListener('click', async () => {
+        const user_id = document.querySelector("#user_id");
+
+        const database_json = {
+            user: user_id.value,
+        };
+
+        await fetch("/admin/remove_user/data", {
+            method: "POST",
+            body: JSON.stringify(database_json)
+        });
+
+        // REMOVING USERS IS NOT IMPLEMENTED IN THE FILE SYSTEM
+
+        location.href = "/admin";
+    });
+}
+
+
 // Edit user
+
+const edit_user_send_button = document.querySelector("#edit_user_send_button");
+
+if (edit_user_send_button != null) {
+    edit_user_send_button.addEventListener('click', async () => {
+        const user_id = document.querySelector("#user_id");
+        const user_password = document.querySelector("#user_password");
+        const user_name = document.querySelector("#user_name");
+        const user_job_name = document.querySelector("#user_job_name");
+        const user_email = document.querySelector("#user_email");
+        const user_phone_number = document.querySelector("#user_phone_number");
+        const user_type = document.querySelector("#user_type");
+
+        const database_json = {
+            user: user_id.value,
+            employee_name: user_name.value,
+            job_name: user_job_name.value,
+            company_name: COMPANY_NAME,
+            email: user_email.value,
+            phone_number: parseInt(user_phone_number.value),
+        };
+
+        await fetch("/admin/edit_user/data", {
+            method: "POST",
+            body: JSON.stringify(database_json)
+        });
+
+        // EDITING USERS IS NOT IMPLEMENTED IN THE FILE SYSTEM
+
+        location.href = "/admin";
+    });
+}
