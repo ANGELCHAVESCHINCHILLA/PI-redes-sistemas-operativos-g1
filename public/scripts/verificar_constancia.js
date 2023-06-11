c65830d03b95733f8f56201f07663eafb826944d
SrDavidC <david.cerdasalvarado@ucr.ac.cr>
2023-06-10 21:07:36 -0600

    fix: 'verificar_constancia' page updated

--- public/scripts/verificar_constancia.js
+++ public/scripts/verificar_constancia.js
@@ -1,41 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
     var buscarBtn = document.getElementById('buscarBtn');
     buscarBtn.addEventListener('click', buscarConstancia);
-
-    function makeFetch(constanciaId) {
-        fetch('/getConst', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ constanciaId: constanciaId })
-        })
-            .then(function(response) {
-                if (response.ok) {
-                    return response.text();
-                } else {
-                    throw new Error('Error en la respuesta del servidor');
-                    return null;
-                }
-            })
-    }
-    function formatInfo(info) {
-        // TODO: finish this according info receiving from the fetch
-        let formattedText;
-        return formattedText;
-    }
-    function buscarConstancia() {
-        var constanciaId = document.getElementById('constanciaId').value;
-        var newWindow = window.open('');
-        newWindow.document.write(constanciaId); // remove this when below TODO is ready
-        // TODO: when DB API rest is up, change this to work
-        /*
-        let info = makeFetch(constanciaId);
-        if (info) {
-            formatInfo(info)
-            newWindow.document.write(constanciaId);
-        }
-
-         */
-    }
 });
+
+function buscarConstancia() {
+    var constanciaId = document.getElementById('constanciaId').value;
+    // var newWindow = window.open('');
+    // Make fetch
+    const url = new URL("http://127.0.0.1:8003/consultRequestsByID");
+    const params = {id: constanciaId};
+    url.search = new URLSearchParams(params).toString();
+    console.log(url);
+    const promise = fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then(response => {
+        console.log("Status: " + response.status )
+        if (response.status == 200) {
+            let newWindow = window.open('');
+            let pageHTML = formatResponseInfo(response.json());
+            newWindow.write(pageHTML);
+        } else {
+             document.write(response.text());
+        }
+    }).catch(error => {
+        console.log(error);
+    })
+
+}
+
+function formatResponseInfo(responseJSON) {
+    let page = "PlaceHolde Info";
+    return page;
+}
