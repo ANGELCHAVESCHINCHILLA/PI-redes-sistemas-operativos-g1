0456644fb77380fe32c48b2c352bed2296e63705
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2023-06-11 15:11:39 -0600

    AddUserHandler split cpp and hpp

--- src/filesystem_app/HttpRequestHandlers/AddUserHandler.hpp
+++ src/filesystem_app/HttpRequestHandlers/AddUserHandler.hpp
@@ -5,61 +5,13 @@
 #ifndef PI_REDES_SISTEMAS_OPERATIVOS_G1_ADDUSERHANDLER_H
 #define PI_REDES_SISTEMAS_OPERATIVOS_G1_ADDUSERHANDLER_H
 
-#include <jsoncpp/json/json.h>
-
 #include "FileSystemRequestHandler.hpp"
-#include "../../common/Log.hpp"
 
 class AddUserHandler : public FileSystemRequestHandler {
  public:
-  explicit AddUserHandler(FileSystemAPI* fileSystemApi) :
-        FileSystemRequestHandler(fileSystemApi){}
+  explicit AddUserHandler(FileSystemAPI* fileSystemApi);
 
-  bool canHandle(HttpRequest& request, HttpResponse& response) override {
-    if (request.getMethod() == "POST" &&
-        request.getTarget().getPath() == "/adduser") {
-
-      Log::getInstance().write(Log::INFO, "AddUserRequestHandled", request.getTarget().getPath());
-      const std::string& body = request.getBody();
-      Json::Value requestBody;
-      Json::Reader reader;
-      // Parse to JSON to easy manipulation
-      bool parsed = reader.parse(body, requestBody);
-
-      int statusCode;
-      std::string responseBody;
-
-      if (parsed) {
-        // Get values
-        std::string username = requestBody["username"].asString();
-        std::string password = requestBody["password"].asString();
-        std::string salt = requestBody["salt"].asString();
-        int type = requestBody["type"].asInt();
-
-        // Auth by API
-        bool isAdded =
-            this->fileSystemApi->addUser(username, password, salt, type);
-
-        statusCode = isAdded ? 200 : 400;
-        responseBody = isAdded ? "Successfully" : "Failed";
-
-      } else {
-        statusCode = 400;
-        responseBody = "JSON ERROR";
-        Log::getInstance().write(Log::ERROR, "AddUserRequest", "JSON Parsing Error");
-      }
-      response.setHeader("Content-Type", "text/plain");
-      response.setStatusCode(statusCode);
-      response.getBody() << responseBody;
-
-      response.buildResponse();
-      // Send
-      Log::getInstance().write(
-          Log::INFO, "ResponseSend", "Response Sent with body:" + responseBody);
-      return true;
-    }
-    return false;
-  }
+  bool canHandle(HttpRequest& request, HttpResponse& response) override;
 };
 
 #endif  // PI_REDES_SISTEMAS_OPERATIVOS_G1_ADDUSERHANDLER_H
