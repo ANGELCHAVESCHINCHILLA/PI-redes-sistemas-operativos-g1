0cb19ae375726c25fdba00c08cb7a7f804eee0a2
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2023-06-11 16:34:02 -0600

    Connect the web app with the fs app to edit and remove users

--- src/filesystem_app/HttpRequestHandlers/RemoveUserHandler.cpp
+++ src/filesystem_app/HttpRequestHandlers/RemoveUserHandler.cpp
@@ -4,6 +4,8 @@
 
 #include <jsoncpp/json/json.h>
 
+#include "../../common/Log.hpp"
+
 RemoveUserHandler::RemoveUserHandler(FileSystemAPI* api)
     : FileSystemRequestHandler(api) {
 }
@@ -15,6 +17,33 @@ bool RemoveUserHandler::canHandle(
     HttpRequest& request, HttpResponse& response) {
   if (request.getMethod() == "DELETE" &&
       request.getTarget().getPath() == "/auth/user") {
+    try {
+      Json::Reader reader;
+      Json::Value root;
+
+      if (!reader.parse(request.getBody(), root)) {
+        throw std::runtime_error("Can't parse the json.");
+      }
+
+      std::string username = root["username"].asString();
+
+      if (!this->fileSystemApi->removeUser(username)) {
+        throw std::runtime_error("User doesn't exist.");
+      }
+
+      response.setStatusCode(200);
+    } catch (const std::runtime_error& error) {
+      Log::getInstance().write(
+          Log::MessageType::WARNING, "RemoveUserHandler", error.what());
+      response.setStatusCode(401);
+      response.getBody() << error.what();
+    } catch (const Json::LogicError& error) {
+      Log::getInstance().write(
+          Log::MessageType::WARNING, "RemoveUserHandler", error.what());
+      response.setStatusCode(401);
+      response.getBody() << error.what();
+    }
+
     return true;
   }
 
